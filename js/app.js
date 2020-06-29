383a8b20f39be922a6bbf3909aa19c3bd190871a
katrina-mathebula <60740241+katrina-mathebula@users.noreply.github.com>
2020-06-29 15:09:46 +0200

    manifest

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/sw.js')
+    .then((reg) => console.log('service worker registered', reg))
+    .catch((err) => console.log('service worker not registered', err));
+}
+
+
 const navbar = document.querySelector("#nav");
 const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
